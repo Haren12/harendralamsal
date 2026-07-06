e3b707d061c728775a8abca4a6ea51e1ba64d9bf
Harendra Lamsal <146507571+Haren12@users.noreply.github.com>
2026-07-06 14:30:58 +0545

    Update category.$slug.tsx

--- src/routes/category.$slug.tsx
+++ src/routes/category.$slug.tsx
@@ -1,3 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
+import { useServerFn } from "@tanstack/react-start";
+import { listPostsByCategory } from "@/lib/blog.functions";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/category/$slug")({
@@ -5,9 +8,20 @@ export const Route = createFileRoute("/category/$slug")({
 });
 
 function CategoryPage() {
+  const { slug } = Route.useParams();
+
+  const getPosts = useServerFn(listPostsByCategory);
+
+  const postsQ = useQuery({
+    queryKey: ["categoryPosts", slug],
+    queryFn: () => getPosts({ data: { slug } }),
+  });
+
   return (
     <div className="container-page py-20">
-      <h1>Category Page</h1>
+      <h1>Category: {slug}</h1>
+
+      <p>Total Posts: {postsQ.data?.length ?? 0}</p>
     </div>
   );
 }
