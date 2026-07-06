f2eb2da4430b3cda23af300bd7b83880017568b8
Harendra Lamsal <146507571+Haren12@users.noreply.github.com>
2026-07-06 14:09:31 +0545

    Create category route

--- src/routes/category.$slug.tsx
+++ src/routes/category.$slug.tsx
@@ -1,1 +1,13 @@
+import { createFileRoute } from "@tanstack/react-router";
 
+export const Route = createFileRoute("/category/$slug")({
+  component: CategoryPage,
+});
+
+function CategoryPage() {
+  return (
+    <div className="container-page py-20">
+      <h1>Category Page</h1>
+    </div>
+  );
+}
